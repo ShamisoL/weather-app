521a78a8e8b89f8d5ab0f03bcdeb5aeefa677e88
ShamisoL <86979110+ShamisoL@users.noreply.github.com>
2021-09-12 14:31:30 +0100

    Added search engine

--- src/app.js
+++ src/app.js
@@ -43,7 +43,17 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "a328a3e71dbdbe04dcb2ea0fc3c1f1df";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "a328a3e71dbdbe04dcb2ea0fc3c1f1df";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
